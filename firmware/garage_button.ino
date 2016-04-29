d99a463709a0f4f4f76dd7c85e7f91a3272af6be
Jason Pilz <jasonpilz@gmail.com>
2016-04-29 17:54:46 -0600

    revise garage_button.ino

--- firmware/garage_button.ino
+++ firmware/garage_button.ino
@@ -1,19 +1,24 @@
 int garagePin = D0;
 
-void setup()
-{
-  pinMode(garagePin, OUTPUT);
-
+void setup() {
   Particle.function("garage", garageButtonPush);
+
+  pinMode(garagePin, OUTPUT);
 
   digitalWrite(garagePin, LOW);
 }
 
-int garageButtonPush() {
-  // Push button for half second (Close contacts) using optocoupler
-  digitalWrite(garagePin, HIGH)
-  delay(500)
+void loop() {}
 
-  // Open button contacts
-  digitalWrite(garagePin, LOW)
+int garageButtonPush(String command) {
+    if(command == "open") {
+      // Push button for half second (Close contacts) using optocoupler
+      digitalWrite(garagePin, HIGH);
+      delay(500);
+      return 1;
+    }
+    else return -1;
+
+    // Open button contacts
+    digitalWrite(garagePin, LOW);
 }
