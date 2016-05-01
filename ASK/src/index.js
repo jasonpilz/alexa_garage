37cc8fa50d1547f18881f618ad38d30f649390a7
Jason Pilz <jasonpilz@gmail.com>
2016-04-30 18:57:52 -0600

    implement index.js

--- ASK/src/index.js
+++ ASK/src/index.js
@@ -7,8 +7,8 @@ var APP_ID      = undefined;
 var deviceId    = undefined;
 var accessToken = undefined;
 
-var hostUrl     = "api.particle.io";
-var deviceUrl   = "/v1/devices";
+var hostUrl     = 'api.particle.io';
+var deviceUrl   = '/v1/devices';
 
 // Garage is a child of AlexaSkill
 let Garage = () => {
@@ -22,7 +22,7 @@ Garage.prototype.constructor = Garage;
 // Handle Events
 Garage.prototype.eventHandlers.onSessionStarted = (sessionStartedRequest, session) => {
   console.log(`Garage onSessionStarted requestId: ${sessionStartedRequest.requestId}
-                                         sessionId: ${session.sessionId}`);
+                                       sessionId: ${session.sessionId}`);
 };
 
 Garage.prototype.eventHandlers.onLaunch = (launchRequest, session, response) => {
@@ -41,5 +41,75 @@ Garage.prototype.eventHandlers.onSessionEnded = (sessionEndedRequest, session) =
 
 // Handle intents
 Garage.prototype.intentHandlers = {
+  ToggleGarageIntent: (intent, session, response) => {
+    let OpenCloseSlot    = intent.slots.OpenClose;
+    let ToggleGaragePath = `${deviceUrl}${deviceId}/garage`;
 
+    if (OpenCloseSlot && OpenCloseSlot.value) {
+      if (OpenCloseSlot == 'open' || OpenCloseSlot == 'close') {
+
+        callParticleApi(ToggleGaragePath, (apiResponse) => {
+          let parsedResponse = JSON.parse(apiResponse);
+          let speechOutput = '';
+
+          if (parsedResponse.return_value == 1) {
+            speechOutput = 'OK, the garage door has obeyed your command';
+          } else if (parsedResponse.return_value == -1) {
+            speechOutput = 'An error has occurred. Blame the developer';
+          }
+          response.tellWithCard(speechOutput, 'Garage');
+        });
+      } else {
+        handleNoSlotDialogRequest(intent, session, response);
+      }
+    }
+  },
+
+  'AMAZON.HelpIntent': (intent, session, response) => {
+    response.ask('You can tell me to open or close the garage door');
+  }
+};
+
+function handleNoSlotDialogRequest(intent, session, response) {
+  // TODO - no slots were given, remind user how to call function
+}
+
+function callParticleApi(path, callback) {
+  let options = {
+    hostname: hostUrl,
+    port: 443,
+    path: path,
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  };
+
+  makeRequest(options, callback);
+}
+
+function makeRequest(options, callback) {
+  let req = https.request(options, (res) => {
+    console.log(`statusCode: ${res.statusCode}`);
+    console.log(`headers: ${res.headers}`);
+
+    let body = '';
+    res.setEncoding('utf8');
+    res.on('data', chunk => { body += chunk; });
+    res.on('end', () => { callback(body); });
+  });
+
+  // write arguments to request body
+  req.write('args=open');
+  req.end();
+  req.on('error', e => { console.error(e); });
+}
+
+// Create the handler that responds to the Alexa Request
+exports.handler = (event, context) => {
+
+    // Create an instance of the Particle skill
+    let garageSkill = new Garage();
+    garageSkill.execute(event, context);
 };
